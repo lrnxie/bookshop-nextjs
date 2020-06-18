fb3334e5ac94f878cb47505b1fd2e409e9b1df5a
Lauren Xie <lrnxie@gmail.com>
2020-06-17 22:12:11 -0400

    Display books in index page

---
+++ contexts/bookData.js
@@ -0,0 +1,42 @@
+export const bookData = [
+  {
+    id: 1,
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    price: 11.99,
+    cover:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657.jpg",
+  },
+  {
+    id: 2,
+    title: "1984",
+    author: "George Orwell",
+    price: 13.99,
+    cover:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1532714506l/40961427._SX318_.jpg",
+  },
+  {
+    id: 3,
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    price: 10.99,
+    cover:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SY475_.jpg",
+  },
+  {
+    id: 4,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    price: 7.99,
+    cover:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1490528560l/4671._SY475_.jpg",
+  },
+  {
+    id: 5,
+    title: "Animal Farm",
+    author: "George Orwell",
+    price: 7.99,
+    cover:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1325861570l/170448.jpg",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,45 @@
-import Layout from "../components/Layout";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-export default function Home() {
+import Layout from "../components/Layout";
+import { bookData } from "../contexts/bookData";
+
+export default function Home({ books }) {
   return (
     <Layout>
-      <p>hello</p>
+      <Container fluid>
+        <Row>
+          {books.map((book) => (
+            <Col sm={6} md={4} lg={3} key={book.id}>
+              <Card className="my-2 mx-auto">
+                <Card.Img
+                  className="card-img mt-1"
+                  variant="top"
+                  src={book.cover}
+                />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    by {book.author}
+                  </Card.Subtitle>
+                  <Card.Text>${book.price}</Card.Text>
+                  <Button variant="primary">Add to Cart</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const books = bookData;
+  return {
+    props: { books },
+  };
 }
